212c1b29eefedebe1f418261d059335235b4792a
rGustave07 <rGustave5@gmail.com>
2019-08-15 10:56:45 -0700

    Logic flow created up to end-game, polish needed

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
+
+export default function App() {
+
+	const [userNumber, setUserNumber] = useState();
+	const [guessRounds, setGuessRounds] = useState(0);
+
+	const startGameHandler = selectedNumber => {
+		setUserNumber(selectedNumber)
+	}
+
+	const gameOverHandler = numberOfRounds => {
+		setGuessRounds(numberOfRounds);
+	}
+
+	let content = <StartGameScreen onStartGame={startGameHandler} />
+
+	if (userNumber && guessRounds <= 0) {
+		content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+	} else if (guessRounds > 0) {
+		content = <GameOverScreen />
+	}
+
+	return (
+		<View style={styles.screen}>
+			<Header title="Guess A Number"/>
+			{content}
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+	}
+});
