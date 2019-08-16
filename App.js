72d4e3aa759f573b277a1028fda0601fdc138deb
rGustave07 <rGustave5@gmail.com>
2019-08-16 09:22:43 -0700

    finished game loop

--- App.js
+++ App.js
@@ -11,6 +11,11 @@ export default function App() {
 	const [userNumber, setUserNumber] = useState();
 	const [guessRounds, setGuessRounds] = useState(0);
 
+	const configureNewGameHandler = () => {
+		setGuessRounds(0);
+		setUserNumber(null);
+	}
+
 	const startGameHandler = selectedNumber => {
 		setUserNumber(selectedNumber)
 	}
@@ -24,7 +29,7 @@ export default function App() {
 	if (userNumber && guessRounds <= 0) {
 		content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
 	} else if (guessRounds > 0) {
-		content = <GameOverScreen />
+		content = <GameOverScreen userNumber={userNumber} roundsNumber={guessRounds} onRestart={configureNewGameHandler}/>
 	}
 
 	return (
